d6f3b11cd4f33422abac96edbc166a17d5053166
Jack Yao <995204576@qq.com>
2014-04-01 16:15:47 +0800

    Update ch2.hs

--- ch2/ch2.hs
+++ ch2/ch2.hs
@@ -5,7 +5,15 @@ import Test.QuickCheck
 msc :: Ord a => [a] -> Int
 msc xs = maximum [scount z zs | z : zs <- tails xs]
 
+{-  tails是Data.List的库函数，复杂度为O(n)，生成列表的所有后缀。可以这样实现
+   tails :: [a] -> [[a]]
+   tails [] = []
+   tails (x : xs) = (x : xs) : tail xs
+-}
+
 scount x xs = length (filter (x <) xs)
+
+
 
 -- d&c O(nlogn)
 msc' :: Ord a => [a] -> Int
