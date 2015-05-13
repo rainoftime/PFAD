46782b04f049900224915f9b9627a88e59ec3d75
Rrandom <emanonhere@gmail.com>
2015-05-13 22:49:56 +0800

    add racket file for chapter 1

--- ch1/ch1.rkt
+++ ch1/ch1.rkt
@@ -10,7 +10,23 @@
     (λ (x) (not (member x xs)))
     (in-naturals))))
 
+(define (minform a n xs)
+  (if (zero? n)
+      a
+      (let ([b (+ 1 a (quotient n 2))])
+        (let-values ([(us vs) (partition (λ (x) (< x b)) xs)])
+          (let ([m (length us)])
+            (if (= m (- b a))
+                (minform b (- n m) vs)
+                (minform a m us)))))))
+
+(define (minfree2 xs)
+  (minform 0 (length xs) xs))
 
 ;; test 
 (define xs (map string->number (string-split "08,23,09,00,12,11,01,10,13,07,41,04,14,21,05,17,03,19,02,06" ",")))
 (minfree xs)
+(minfree2 xs)
+
+;; need more tests
+
